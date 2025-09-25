8e474766dfadc83a0c29c3d1ed846c1239af040a
sarathiU11 <sarathiu11@gmail.com>
2025-09-25 22:56:09 +0530

    day 2  print function

---
+++ day2 of AI & ML.py
@@ -0,0 +1,20 @@
+print("Hello programmer")
+
+print("the number is ",4)
+print(" 2 + 2 =" ,4)
+
+name = "sarathi"
+print(f"Hello,{name}! Welcome to python programming")
+
+name1 = input("what is your name ?")
+print(f"hello ,{name1}! welcome to the competition world")
+
+#project 1 #
+
+User_name=input("what is your name?")
+hobby = input("what is your hobby ?")
+
+print("/n ----welcome message---")
+print(f"hello  {User_name}")
+print("welcome to programming ")
+print(f"that great{hobby} hobby")
