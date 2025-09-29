79203284938cd343bf3c1d6f4112a54cc16fb2de
sarathiU11 <sarathiu11@gmail.com>
2025-09-29 22:52:51 +0530

    Day 3: Control flow + patterns

---
+++ day4 of AI & ML.py
@@ -0,0 +1,47 @@
+name = input("enter you name: ")
+print(f"hello, {name}")
+
+num1 = input("enter the number 1: ")
+num1= int(num1)
+print(f"number doubled : {num1 * 2}")
+
+num2 = int(input("enter the number: "))
+result = num1 + num2
+
+print(f"the sum of {num1} and {num2} is {result}")
+
+#arithemetic  operations
+
+a = 10
+b = 5
+
+print(f"addition : { a+b}")
+print(
+    f"subtraction : { a - b}"
+)
+print(f"multipication : {a * b}")
+print(f"division : {a/b}")
+print(f"floor division :{a//b}")
+print(f"modulus: {a % b}")
+print(f"exponentiation: {a ** b}")
+
+# project 3 : simple calculator
+
+n1=int(input("enter the number:"))
+n2=int(input("enter the number 2: "))
+
+
+add=n1+n2
+sub=n1-n2
+mul=n1*n2
+div = n1/n2
+
+
+print("\n -------calculator resuults--------")
+print(f"addition : { add}")
+print(
+    f"subtraction : { sub}"
+)
+print(f"multipication : {mul}")
+print(f"division : {div}")
+
